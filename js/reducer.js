1ec1ec0b6a5b2206a6b4af0db78f5daae6201986
tolentinoel <rckrleyn@gmail.com>
2021-01-04 23:25:45 -0800

    done

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,20 @@
+let state = {count: 0};
+
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
+
+function dispatch(action){
+  state = changeState(state, action)
+  render()
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function render(){
+  document.body.textContent = state.count
+}
 
-changeState(state, action)
